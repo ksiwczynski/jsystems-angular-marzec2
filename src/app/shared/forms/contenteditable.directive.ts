601bf7c45b3b6183223fc212f777a893513a54c0
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 15:18:06 +0100

    contentediable NG_VALUE_ACCESSOR InjectionToken

---
+++ src/app/shared/forms/contenteditable.directive.ts
@@ -0,0 +1,43 @@
+import { Directive, ElementRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  NgModel,
+} from '@angular/forms';
+
+// interface Placki {} // Removed during compilation
+
+@Directive({
+  selector: '[contenteditable=true][ngModel]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ContenteditableDirective,
+      multi: true, // InjectionToken<T[]> // -> Array<T>
+    },
+  ],
+})
+export class ContenteditableDirective implements ControlValueAccessor {
+  constructor(
+    // private model: NgModel,  /// <<--- Cyclical dep
+    private elem: ElementRef<HTMLElement>
+  ) {
+    console.log(elem);
+  }
+
+  writeValue(obj: any): void {
+    this.elem.nativeElement.innerHTML = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.elem.nativeElement.addEventListener('input', (e) =>
+      fn(this.elem.nativeElement.innerHTML)
+    );
+  }
+  registerOnTouched(fn: any): void {
+    this.elem.nativeElement.addEventListener('blur', (e) => fn());
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.elem.nativeElement.contentEditable = isDisabled ? 'false' : 'true';
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { FormsModule } from '@angular/forms';
 import { ClockComponent } from './components/clock/clock.component';
+import { ContenteditableDirective } from './forms/contenteditable.directive';
 
 @NgModule({
-  declarations: [ClockComponent],
+  declarations: [ClockComponent, ContenteditableDirective],
   imports: [MatListModule, FormsModule, CommonModule],
-  exports: [MatListModule, FormsModule, CommonModule, ClockComponent],
+  exports: [MatListModule, FormsModule, CommonModule, ClockComponent, ContenteditableDirective],
 })
 export class SharedModule {}
