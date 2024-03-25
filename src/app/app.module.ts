b5dac33f210f3074374481db7f40fb1859b177a5
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 10:48:50 +0100

    NgModule, Component, Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
+
+// export class AppModule implements DoBootstrap {
+//   ngDoBootstrap(appRef: ApplicationRef): void {
+//     appRef.bootstrap(AppComponent, ['app-root']);
+//   }
+// }
