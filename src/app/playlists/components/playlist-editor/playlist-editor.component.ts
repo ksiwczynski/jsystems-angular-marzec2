00feab5c98787bc21f4636d448cdfd94760c6049
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 14:25:24 +0100

    Casting vs type narrowing

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -4,10 +4,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
   styleUrl: './playlist-editor.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush // ♪ ♫ (( d[-_-]b )) ♪ ♫
+  changeDetection: ChangeDetectionStrategy.OnPush, // ♪ ♫ (( d[-_-]b )) ♪ ♫
 })
 export class PlaylistEditorComponent {
-
   playlist = {
     id: '123',
     name: 'Playlist 123',
@@ -15,5 +14,11 @@ export class PlaylistEditorComponent {
     description: 'Long description...',
   };
 
-  
+  updateName($event: Event) {
+    // this.playlist.name = ($event.target as HTMLInputElement).value;
+
+    if ($event.target instanceof HTMLInputElement) {
+      this.playlist.name = $event.target.value;
+    }
+  }
 }
