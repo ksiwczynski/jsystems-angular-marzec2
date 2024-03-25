fb341320f2ded0f4d333d411686c4d42afb01923
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 12:16:31 +0100

    Exercise: Interpolate mock playlist data

--- src/app/playlists/components/playlist-details/playlist-details.component.ts
+++ src/app/playlists/components/playlist-details/playlist-details.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlist-details',
   templateUrl: './playlist-details.component.html',
-  styleUrl: './playlist-details.component.scss'
+  styleUrl: './playlist-details.component.scss',
 })
 export class PlaylistDetailsComponent {
+
+  playlist = {
+    id: '123',
+    name: 'Playlist 123',
+    public: false,
+    description: 'Long description...',
+  };
 
 }
