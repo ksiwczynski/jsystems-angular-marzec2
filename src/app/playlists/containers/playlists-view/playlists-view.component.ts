8a1e0fe08a1f100fa7053fde4a15b4d161613aaa
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 15:40:06 +0100

    Exercise: switching modes

--- src/app/playlists/containers/playlists-view/playlists-view.component.ts
+++ src/app/playlists/containers/playlists-view/playlists-view.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlists-view',
   templateUrl: './playlists-view.component.html',
-  styleUrl: './playlists-view.component.scss'
+  styleUrl: './playlists-view.component.scss',
 })
 export class PlaylistsViewComponent {
+  mode: 'details' | 'editor' = 'details';
 
+  showDetails() {}
+  showEditor() {}
 }
