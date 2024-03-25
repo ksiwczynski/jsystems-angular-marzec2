4667d1e00141a6a1f743c6ac439763c6abe85ae8
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 14:14:19 +0100

    ChangeDetectionStrategy.OnPush // (( d[-_-]b ))

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
   styleUrl: './playlist-editor.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush // (( d[-_-]b ))
 })
 export class PlaylistEditorComponent {
 
