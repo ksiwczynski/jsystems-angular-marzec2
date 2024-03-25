5722788a7c7b28af82a0b36084c4566ad851f243
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-25 13:48:10 +0100

    Exercise: Fill forms with initial playlist values

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
-  styleUrl: './playlist-editor.component.scss'
+  styleUrl: './playlist-editor.component.scss',
 })
 export class PlaylistEditorComponent {
-
+  playlist = {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Long description...',
+  };
 }
