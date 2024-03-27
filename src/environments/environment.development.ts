175a33c97f488efa3f38190ef904498ad55936e0
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-27 10:44:42 +0100

    SSR + Prerender

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,36 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+  // Url of the Identity Provider
+  // issuer: 'https://idsvr4.azurewebsites.net',
+  oidc: false,
+
+  loginUrl: 'https://accounts.spotify.com/authorize',
+
+  // URL of the SPA to redirect the user to after login
+  redirectUri: (globalThis).location?.origin + '/index.html',
+
+  // The SPA's id. The SPA is registerd with this id at the auth-server
+  // clientId: 'server.code',
+  clientId: '???????????????',
+
+  // Just needed if your auth server demands a secret. In general, this
+  // is a sign that the auth server is not configured with SPAs in mind
+  // and it might not enforce further best practices vital for security
+  // such applications.
+  // dummyClientSecret: 'secret',
+  responseType: 'token',
+
+  // set the scope for the permissions the client should request
+  // The first four are defined by OIDC.
+  // Important: Request offline_access to get a refresh token
+  // The api scope is a usecase specific one
+  scope: 'user-read-private user-read-email',
+
+  showDebugInformation: true,
+};
+
 export const environment = {
   api_url: 'https://api.spotify.com/',
+  authConfig,
 };
