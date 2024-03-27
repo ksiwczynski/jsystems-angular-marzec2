e9ffb5947a57878cf69983e5181b953b4f4d0081
Mateusz Kulesza <ev45ive@gmail.com>
2024-03-27 09:38:20 +0100

    ng g environments + fileReplacement

--- src/app/core/services/music-api.service.ts
+++ src/app/core/services/music-api.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { mockAlbums } from './mockAlbums';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MusicApiService {
+
+  api_url = environment.api_url;
+
   searchAlbums(query = 'batman') {
     console.log(query);
 
